2371bf27c734f8a1905aee3d57a484b786f400b2
augustinandrea <augustin.andrea@gmail.com>
2017-04-17 17:52:43 -0400

    commit

--- Cavalier.cpp
+++ Cavalier.cpp
@@ -9,17 +9,18 @@
 using namespace std;
 
 Cavaleir::Cavalier (string Name, string Class, bool dead) : Character (Name, Class, dead)
-{ Name = "Stahl";
+{ Name_1 = "Stahl";
+  Name_2 = "Sully";
 }
 
 void Cavalier::upgradeClass() {
 	Class = "Paladin";
 	stats["HP"]= stats["Max HP"];                  // The amount of hp a character currently has
-	stats["Max HP"]= 30;		 // The total amount of hp a character has in total
+	stats["Max HP"]= 42;		 // The total amount of hp a character has in total
 	stats["Avoid"]= stats["Avoid"] + 5;		 // The percentage chance that an unit has in dodging a character's attack
-	stats["Accuracy"]= stats["Accuracy"] + 5;      // The percentage chance that an unit's attacks hit
-	stats["Damage"]= 12;		 // The amount of damage an attack does
-	stats["Crit Damage"]= 18;
+	stats["Accuracy"]= stats["Accuracy"] + 3;      // The percentage chance that an unit's attacks hit
+	stats["Damage"]= 11;		 // The amount of damage an attack does
+	stats["Crit Damage"]= stats["Crit Damage"] + 6;
 }
 
 void Cavalier::print()
@@ -29,9 +30,9 @@ void Cavalier::print()
 	cout << "Class: " << Class << endl;
 	stats["HP"]= stats["Max HP"];                  // The amount of hp a character currently has
 	stats["Max HP"]= 22;		 // The total amount of hp a character has in total
-	stats["Avoid"]= 50;		 // The percentage chance that an unit has in dodging a character's attack
-	stats["Accuracy"]= 55;      // The percentage chance that an unit's attacks hits
-	stats["Crit"]= 55;		 // The percentage chance that an unit's attack does crit damage
+	stats["Avoid"]= 65;		 // The percentage chance that an unit has in dodging a character's attack
+	stats["Accuracy"]= 87;      // The percentage chance that an unit's attacks hits
+	stats["Crit"]= 50;		 // The percentage chance that an unit's attack does crit damage
 	stats["Damage"]= 8;		 // The amount of damage an attack does
-	stats["Crit Damage"]= 12;
+	stats["Crit Damage"]= 18;
 }
--- DarkMage.cpp
+++ DarkMage.cpp
@@ -8,16 +8,31 @@
 #include <string>
 using namespace std;
 
-DarkMage::DarkMage (string Name, string Class, bool dead) : Character (Name, Class, dead)
-{}
+DarkMage::DarkMage (string Name, string Class, bool dead) : Character (Name, Class, dead) {
+  Name = "Henry", "Tharja";
+}
 
-void DarkMage::upgradeClass()
-{Class = "Sorcerer";}
+void DarkMage::upgradeClass() {
+  Class = "Sorcerer";
+  stats["HP"]= stats["Max HP"];              // The amount of hp a character currently has                                                        
+  stats["Max HP"]= 32;        // The total amount of hp a character has in total                                                                  
+  stats["Avoid"]= stats["Avoid"] + 5;         // The percentage chance that an unit has in dodging a character's attack                                           
+  stats["Accuracy"]= stats["Accuracy"] + 3;      // The percentage chance that an unit's attacks hits                                                                
+  stats["Crit"]= stats["Crit"] + 10;          // The percentage chance that an unit's attack does crit damage                                                     
+  stats["Damage"]= stats["Damage"] + 3;        // The amount of damage an attack does                                                                              
+  stats["Crit Damage"]= stats["Crit Damage"] + 6;
+}
 
 void DarkMage::print()
 {
 	cout << Name << endl;
 	cout << ========================================= << endl;
 	cout << "Class: " << Class << endl;
-	//DISPLAY STATS FROM MAP
+        stats["HP"]= stats["Max HP"];              // The amount of hp a character currently has
+        stats["Max HP"]= 17;        // The total amount of hp a character has in total 
+        stats["Avoid"]= 55;         // The percentage chance that an unit has in dodging a character's attack
+        stats["Accuracy"]= 80;      // The percentage chance that an unit's attacks hits 
+        stats["Crit"]= 40;          // The percentage chance that an unit's attack does crit damage                                                
+        stats["Damage"]= 10;        // The amount of damage an attack does
+        stats["Crit Damage"]= 18;
 }
