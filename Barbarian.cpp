7f7a49fca039002d22f48c4cbc0646d1b19566eb
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 13:16:26 -0400

    Tester works!

--- Barbarian.cpp
+++ Barbarian.cpp
@@ -8,7 +8,7 @@
 #include <string>
 using namespace std;
 
-Barbarian::Barbarian (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead)
+Barbarian::Barbarian (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead, hp, maxHP, avoid, accuracy, crit, damage, critDamage, heal)
 {}
 
 void Barbarian::upgradeClass()
