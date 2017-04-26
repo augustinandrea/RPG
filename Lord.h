824c7ee98ac7b5e61faefa18585f212cf5df9fba
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 09:41:13 -0400

    compiles

--- Lord.h
+++ Lord.h
@@ -16,8 +16,10 @@ class Lord : public Character
 		Lord(string="Name", string="Lord", bool=false, int=0, int=0, int=0, int=0, int=0, int=0, int=0, int=0);
 		virtual void upgradeClass();
 		virtual void displayInfo();
-		//Any methods a Lord may have that other classes won't
 	private:
-		//Anything a Lord may have that other classes won't
+		string Name;
+		string Class;
+		bool dead;
+		map<string,int> stats;
 };
 #endif
