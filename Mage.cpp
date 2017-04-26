bdab3ef84d35b1e4aa7ac8da0953816f0d0f497c
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 13:17:05 -0400

    Tester works!

--- Mage.cpp
+++ Mage.cpp
@@ -8,7 +8,7 @@
 #include <string>
 using namespace std;
 
-Mage::Mage (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead)
+Mage::Mage (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead, hp, maxHP, avoid, accuracy, crit, damage, critDamage, heal)
 {}
 
 void Mage::upgradeClass()
