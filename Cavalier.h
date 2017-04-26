a60e148b94c14413d155cd1115d6a4573a4dd39a
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 09:40:14 -0400

    compiles

--- Cavalier.h
+++ Cavalier.h
@@ -16,8 +16,10 @@ class Cavalier : public Character
 		Cavalier(string="Name", string="Cavalier", bool=false, int=0, int=0, int=0, int=0, int=0, int=0, int=0, int=0);
 		virtual void upgradeClass();
 		virtual void displayInfo();
-		//Any methods a Lord may have that other classes won't
 	private:
-		//Anything a Lord may have that other classes won't
+		string Name;
+		string Class;
+		bool dead;
+		map<string,int> stats;
 };
 #endif
