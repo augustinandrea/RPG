433f1e010106f429e952a770de8291b2d5055934
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 13:16:37 -0400

    Tester works!

--- Cleric.cpp
+++ Cleric.cpp
@@ -8,7 +8,7 @@
 #include <string>
 using namespace std;
 
-Cleric::Cleric (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead) 
+Cleric::Cleric (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead, hp, maxHP, avoid, accuracy, crit, damage, critDamage, heal) 
 {}
 
 void Cleric::upgradeClass() {
