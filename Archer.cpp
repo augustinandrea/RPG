5fb047aaf792e2f3af832b678f36fc10521c9d3f
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 13:16:20 -0400

    Tester works!

--- Archer.cpp
+++ Archer.cpp
@@ -8,7 +8,7 @@
 #include <string>
 using namespace std;
 
-Archer::Archer(string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead)
+Archer::Archer(string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead, hp, maxHP, avoid, accuracy, crit, damage, critDamage, heal)
 {}
 
 void Archer::upgradeClass() {
