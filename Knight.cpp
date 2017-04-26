73a29df404339e9a10eac0c7d3b17a9c997ff103
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 13:16:55 -0400

    Tester works!

--- Knight.cpp
+++ Knight.cpp
@@ -8,7 +8,7 @@
 #include <string>
 using namespace std;
 
-Knight::Knight (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead)
+Knight::Knight (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead, hp, maxHP, avoid, accuracy, crit, damage, critDamage, heal)
 {}
 
 void Knight::upgradeClass()
