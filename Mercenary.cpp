c910b7570ff921a67c6d3c62f88e521446a79233
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 09:41:29 -0400

    compiles

--- Mercenary.cpp
+++ Mercenary.cpp
@@ -8,16 +8,16 @@
 #include <string>
 using namespace std;
 
-Mercenary::Mercenary (string Name, string Class, bool dead) : Character (Name, Class, dead)
+Mercenary::Mercenary (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead)
 {}
 
 void Mercenary::upgradeClass()
 {Class = "Hero";}
 
-void Mercenary::print()
+void Mercenary::displayInfo()
 {
 	cout << Name << endl;
-	cout << ========================================= << endl;
+	cout << "=========================================" << endl;
 	cout << "Class: " << Class << endl;
 	//DISPLAY STATS FROM MAP
 	cout << "HP: " << stats["Max HP"] << endl;; 
