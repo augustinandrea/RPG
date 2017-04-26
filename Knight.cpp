3ad1eab2f48d7b38b3296bdb4dc140f88e2111d6
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 09:41:05 -0400

    compiles

--- Knight.cpp
+++ Knight.cpp
@@ -8,16 +8,16 @@
 #include <string>
 using namespace std;
 
-Knight::Knight (string Name, string Class, bool dead) : Character (Name, Class, dead)
+Knight::Knight (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead)
 {}
 
 void Knight::upgradeClass()
 {Class = "General";}
 
-void Knight::print()
+void Knight::displayInfo()
 {
 	cout << Name << endl;
-	cout << ========================================= << endl;
+	cout << "=========================================" << endl;
 	cout << "Class: " << Class << endl;
 	//DISPLAY STATS FROM MAP
 	cout << "HP: " << stats["Max HP"] << endl;; 
