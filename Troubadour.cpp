4324bd79c631c1cb84db39801be8d52993897c89
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 09:42:30 -0400

    compiles

--- Troubadour.cpp
+++ Troubadour.cpp
@@ -8,16 +8,16 @@
 #include <string>
 using namespace std;
 
-Troubadour::Troubadour (string Name, string Class, bool dead) : Character (Name, Class, dead)
+Troubadour::Troubadour (string Name, string Class, bool dead, int hp, int maxHP, int avoid, int accuracy, int crit, int damage, int critDamage, int heal) : Character (Name, Class, dead)
 {}
 
 void Troubadour::upgradeClass()
 {Class = "Valkyrie";}
 
-void Troubadour::print()
+void Troubadour::displayInfo()
 {
 	cout << Name << endl;
-	cout << ========================================= << endl;
+	cout << "=========================================" << endl;
 	cout << "Class: " << Class << endl;
 	//DISPLAY STATS FROM MAP
 	cout << "HP: " << stats["Max HP"] << endl;; 
