631dcc11775a9ae908336804319981a1080118f5
Emily Ying Koykka <ekoykka@student03.cse.nd.edu>
2017-04-26 09:41:32 -0400

    compiles

--- Mercenary.h
+++ Mercenary.h
@@ -16,8 +16,10 @@ class Mercenary : public Character
 		Mercenary(string="Name", string="Mercenary", bool=false, int=0, int=0, int=0, int=0, int=0, int=0, int=0, int=0);
 		virtual void upgradeClass();
 		virtual void displayInfo();
-		//Any methods a Lord may have that other classes won't
 	private:
-		//Anything a Lord may have that other classes won't
+		string Name;
+		string Class;
+		bool dead;
+		map<string,int> stats;
 };
 #endif
