2eb0281fd63479c90a07e2d3d71486a98b43a3f2
augustinandrea <augustin.andrea@gmail.com>
2017-05-03 20:59:45 -0400

    enemy

--- EnemyTurn.cpp
+++ EnemyTurn.cpp
@@ -1,4 +1,4 @@
-
+#include <cmath>
 
 void enemyTurn() {
   string Class; // the class
@@ -6,7 +6,7 @@ void enemyTurn() {
   int Erow;
   bool EturnOver = false; // enemy turn over
   bool EcanGo[6] = {true, true, true, true, true, true};
-
+  
   while(!EturnOver){
 
     if(!EcanGo[0] && !EcanGo[1] && !EcanGo[2] && !EcanGo[3] && !EcanGo[4] && !EcanGo[5]) {
@@ -15,53 +15,52 @@ void enemyTurn() {
       break;
     }
 
-		// based off of around lines 300
+    // based off of around lines 300
     int EnumMoves = 3;
-		bool moving = true;
-
-		cout << endl;
-		for(r=0; r<8; r++)
-		{
-			for(c=0; c<8; c++)
-			{
-				if(2<=d[r][c] && d[r][c]<=7 && canGo[d[r][c]-2])
-				{
-					cout << enemies[d[r][c]-2].getName() << " (" << r << ", " << char(c+65) << ") ";
-				}
-			}
-		}
-		cout << endl;
-
-    // loop through board && get position for each enemy
-    /*
-    for(int i = 0; i < 6; i++){
-      for(int j = 0; j < 6; j++){
-	       if(enemies[i].getName().compare("Enemy" + to_string(i) ) == 0 ){
-	          Erow = i;
-	          Ecol = j;
-         }
+    bool moving = true;
+    
+    cout << endl;
+    for(r=0; r < 8; r++){
+      for(c=0; c < 8; c++){
+	if(2<=d[r][c] && d[r][c]<=7 && canGo[d[r][c]-2]){
+	    cout << enemies[d[r][c]-2].getName() << " (" << r << ", " << char(c+65) << ") ";
+	}
       }
     }
-    */
-
+    cout << endl;
+    
     //get position for each enemy
     for(int i = 0; i < 6; i++){
       for(int j = 0; j < 6; j++){
-	       if(member == d[i][j]){
-	          Prow = i;
-	          Pcol = j;
-	       }
-
-         for(int k = 0; k < 4; k++){
-           if((d[i][j-k] || d[i][j+k] || d[i-k][j] || d[i+k][j]) != 0){
-     	       if(enemies[]){
+	if(member == d[i][j]){
+	  Prow = i;
+	  Pcol = j;
+	}
+	
+	for(int k = 0; k < 4; k++){
+	  if( (d[i][j-k] || d[i][j+k] || d[i-k][j] || d[i+k][j]) != 0){
+	    if(enemies[]){
+	    }
+	  }
+	  
+	}
       }
     }
 
     // enemy distance to player characters
+    for(int k = 0; k < 4; k++){
+      
+    }
 
+    // Enemy clerics/troubadours only go around and heal   
+    // distance formula
+    double x;
+    double y;
+    double a;
+    double b;
+    double distance = sqrt( pow(x - a, 2) + pow(y - b, 2);
 
-    /*
+      /*
     if( (Character.getClass() == "Cleric" || Character.getClass() == "Troubadour") ) {
       if(Character.Cleric < Character.Troubadour){  //attack the Cleric
 
@@ -75,7 +74,7 @@ void enemyTurn() {
 
     }
     */
-    else if(playerCharacter <= 3 && (Cleric || Troubadour) > 3){   // attack the character that is closest
+	  else if(playerCharacter <= 3 && (Cleric || Troubadour) > 3){   // attack the character that is closest
 
     }
 
